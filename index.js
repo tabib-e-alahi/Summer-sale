d0374f08fded6520c3e182956c892d1408abb90f
tabib-e-alahi <etabib.alahi@gmail.com>
2023-08-20 02:48:03 +0600

    all cards added with product name adding feature

--- index.js
+++ index.js
@@ -4,3 +4,59 @@ document.getElementById("card-1").addEventListener("click", function () {
   //    console.log(productName);
   setProductName(productName);
 });
+
+document.getElementById("card-2").addEventListener("click", function () {
+
+  const productName = getInnerTextOfElement("card-2-title");
+  //    console.log(productName);
+  setProductName(productName);
+});
+
+document.getElementById("card-3").addEventListener("click", function () {
+
+  const productName = getInnerTextOfElement("card-3-title");
+  //    console.log(productName);
+  setProductName(productName);
+});
+
+document.getElementById("card-4").addEventListener("click", function () {
+    
+  const productName = getInnerTextOfElement("card-4-title");
+  //    console.log(productName);
+  setProductName(productName);
+});
+
+document.getElementById("card-5").addEventListener("click", function () {
+
+  const productName = getInnerTextOfElement("card-5-title");
+  //    console.log(productName);
+  setProductName(productName);
+});
+
+document.getElementById("card-6").addEventListener("click", function () {
+
+  const productName = getInnerTextOfElement("card-6-title");
+  //    console.log(productName);
+  setProductName(productName);
+});
+
+document.getElementById("card-7").addEventListener("click", function () {
+
+  const productName = getInnerTextOfElement("card-7-title");
+  //    console.log(productName);
+  setProductName(productName);
+});
+
+document.getElementById("card-8").addEventListener("click", function () {
+
+  const productName = getInnerTextOfElement("card-8-title");
+  //    console.log(productName);
+  setProductName(productName);
+});
+
+document.getElementById("card-9").addEventListener("click", function () {
+
+  const productName = getInnerTextOfElement("card-9-title");
+  //    console.log(productName);
+  setProductName(productName);
+});
