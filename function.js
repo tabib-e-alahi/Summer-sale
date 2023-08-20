5212a8b55c9b1691ac92d0b857d01883b8bf7f0d
tabib-e-alahi <etabib.alahi@gmail.com>
2023-08-20 17:18:09 +0600

    dicount calculations done

--- function.js
+++ function.js
@@ -1,3 +1,5 @@
+let totalPrice = 0;
+
 function getInnerTextOfElement(eleId) {
   const element = document.getElementById(eleId);
   const elementText = element.innerText;
@@ -14,4 +16,41 @@ function setProductName(productName) {
     <h1>${count + 1}. ${productName}</h1>
 `;
   targetElementParent.appendChild(h1);
+}
+
+
+function getPriceFromElement(eleId){
+  const element = document.getElementById(eleId);
+  const elementPriceString = element.innerText;
+  const parsedElementPrice = parseFloat(elementPriceString);
+  const elementPrice = parsedElementPrice;
+
+  return elementPrice;
+}
+
+function setProductPrice(productPrice){
+  const targetElement = document.getElementById('total-price');
+  totalPrice += productPrice;
+  if( totalPrice > 0){
+    document.getElementById('make-purchase-btn').disabled = false;
+  }
+  if(totalPrice >= 200){
+    document.getElementById('apply-btn').disabled = false;
+  }
+  targetElement.innerText = totalPrice.toFixed(2);
+  
+}
+
+function makeCalculation(){
+  const totalPrice = getPriceFromElement('total-price');
+  // console.log(typeof totalPrice, totalPrice);
+  const discountedPrice = (totalPrice *20)/100;
+  const newTotal = totalPrice - discountedPrice;
+
+  const discountedPriceElement = document.getElementById('discount-price');
+  discountedPriceElement.innerText = discountedPrice.toFixed(2);
+  
+  const newTotalPriceElement = document.getElementById('after-discount-total');
+  newTotalPriceElement.innerText = newTotal.toFixed(2);
+
 }
--- index.js
+++ index.js
@@ -1,62 +1,75 @@
+
+// ======================== cards function area =====================
 document.getElementById("card-1").addEventListener("click", function () {
-    
-  const productName = getInnerTextOfElement("card-1-title");
-  //    console.log(productName);
-  setProductName(productName);
+  reduceCode("card-1-title", "card-1-price");
 });
+
+
 
 document.getElementById("card-2").addEventListener("click", function () {
-
-  const productName = getInnerTextOfElement("card-2-title");
-  //    console.log(productName);
-  setProductName(productName);
+  reduceCode("card-2-title", "card-2-price");
 });
+
+
 
 document.getElementById("card-3").addEventListener("click", function () {
-
-  const productName = getInnerTextOfElement("card-3-title");
-  //    console.log(productName);
-  setProductName(productName);
+  reduceCode("card-3-title", "card-3-price");
 });
+
+
 
 document.getElementById("card-4").addEventListener("click", function () {
-    
-  const productName = getInnerTextOfElement("card-4-title");
-  //    console.log(productName);
-  setProductName(productName);
+  reduceCode("card-4-title", "card-4-price");
 });
+
+
 
 document.getElementById("card-5").addEventListener("click", function () {
-
-  const productName = getInnerTextOfElement("card-5-title");
-  //    console.log(productName);
-  setProductName(productName);
+  reduceCode("card-5-title", "card-5-price");
 });
+
 
 document.getElementById("card-6").addEventListener("click", function () {
-
-  const productName = getInnerTextOfElement("card-6-title");
-  //    console.log(productName);
-  setProductName(productName);
+  reduceCode("card-6-title", "card-6-price");
 });
+
 
 document.getElementById("card-7").addEventListener("click", function () {
-
-  const productName = getInnerTextOfElement("card-7-title");
-  //    console.log(productName);
-  setProductName(productName);
+  reduceCode("card-7-title", "card-7-price");
 });
+
+
 
 document.getElementById("card-8").addEventListener("click", function () {
-
-  const productName = getInnerTextOfElement("card-8-title");
-  //    console.log(productName);
-  setProductName(productName);
+  reduceCode("card-8-title", "card-8-price");
 });
+
+
 
 document.getElementById("card-9").addEventListener("click", function () {
-
-  const productName = getInnerTextOfElement("card-9-title");
-  //    console.log(productName);
-  setProductName(productName);
+  reduceCode("card-9-title", "card-9-price");
 });
+
+// ======================== cards function area  ended===================== 
+
+document.getElementById('apply-btn').addEventListener('click',function(){
+  const couponInputField = document.getElementById('coupon-code-field');
+  const couponCode = couponInputField.value;
+
+  if (couponCode === 'SELL200'){
+    document.getElementById('invalid-coupon').classList.add('hidden');
+    makeCalculation(); 
+  }
+  else{
+    document.getElementById('invalid-coupon').classList.remove('hidden'); 
+  }
+  
+})
+
+function reduceCode(nameEleId, priceEleId) {
+  const productName = getInnerTextOfElement(nameEleId);
+  setProductName(productName);
+
+  const productPrice = getPriceFromElement(priceEleId);
+  setProductPrice(productPrice);
+}
